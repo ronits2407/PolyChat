6a0e7b98b85aa36b2ff9842e1fa83321af9a9a04
ronits2407 <ronitsonawane2007@gmail.com>
2025-12-30 20:53:50 +0530

    testing done using olama successfully

--- backend/api/controller.js
+++ backend/api/controller.js
@@ -1,8 +1,30 @@
+const OpenAI = require("openai");
+
+const { GoogleGenAI } = require("@google/genai");
+const { json } = require("express");
+const client = new OpenAI();
+const ai = new GoogleGenAI({});
+
 const handleChat = async (req, res) => {
   const { message, model } = req.body;
 
   try {
-    // TODO: Implement chat logic
+    const { message } = req.body;
+
+    const query = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        model: model, // or whichever model you pulled
+        prompt: message,
+        stream: false,
+      }),
+    };
+
+    const response = await fetch("http://localhost:11434/api/generate",query);
+
+    const data = await response.json();
+    res.status(200).json(data);
   } catch (error) {
     res
       .status(500)
