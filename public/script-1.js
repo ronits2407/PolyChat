e1eae1bfc2a628e51bd34a73d8c43f5c16308c16
ronits2407 <ronitsonawane2007@gmail.com>
2025-12-31 15:48:26 +0530

    screen-one completely done using get request to screnn 2

--- public/script-1.js
+++ public/script-1.js
@@ -1,45 +1,56 @@
-const chatarea1 = document.querySelector("#chatarea-1")
-const inputbox = chatarea1.querySelector("#prompt-text")
+//================API functins===========
+const inputbox = document.querySelector("#prompt-text");
+const sendbutton = document.querySelector(".send")
 
-async function gotoscreen2(question) {
-  //${encodeURIComponent(question)}
-  window.location.replace(`https://localhost:5500`)
+function gotoscreen2(question) {
+  window.location.replace(`${window.location.origin}/screen2.html?query=${encodeURIComponent(question)}`);
 }
 
 inputbox.addEventListener("keydown", (evt) => {
   if (evt.key != "Enter") {
-    return
+    return;
   }
-  let content = inputbox.textContent;
-  if (!textContent) {
-    return
+  let content = inputbox.value;
+  if (!content) {
+    return;
+  }
+  
+  gotoscreen2(content);
+});
+
+sendbutton.addEventListener("click", () => {
+  let content = inputbox.value;
+  if (!content) {
+    return;
   }
 
-  
-  
+  gotoscreen2(content)
+
 })
 
 
 
+//=========================
 
+//============== Responsive UI===================
 const sidepanetoggle = document.querySelector("#sidepane-toggle");
-const panel = document.querySelector("#panel")
+const panel = document.querySelector("#panel");
 
 sidepanetoggle.addEventListener("click", () => {
-  console.log("Toggle button was clicked")
+  console.log("Toggle button was clicked");
   if (panel.style.display == "flex") {
-    panel.style.display = "none"
+    panel.style.display = "none";
+  } else {
+    panel.style.display = "flex";
   }
-  else{
-    panel.style.display = "flex"
-  }
-})
+});
 
 window.addEventListener("resize", () => {
   if (window.innerWidth < 768) {
-     panel.style.display = "none"
+    panel.style.display = "none";
+  } else {
+    panel.style.display = "flex";
   }
-  else{
-    panel.style.display = "flex"
-  }
-})
+});
+
+//==========================================
